46fd897cec2e0829a045c051222fe865dbfb6564
Gio85 <giovanni.galiero85@gmail.com>
2021-05-01 13:04:36 +0100

    style(navbar): update style

--- src/components/features/NavBar.tsx
+++ src/components/features/NavBar.tsx
@@ -19,10 +19,15 @@ const StyledForm = styled.form`
 const StyledTitle = styled.h1`
   font-size: 1.8rem;
   margin: 5px;
+  width: 92px;
   font-style: italic;
-  color: ${COLORS.TITLE};
-  text-shadow: 1px 0 0 ${COLORS.WHITE}, -1px 0 0 ${COLORS.WHITE}, 0 1px 0 ${COLORS.WHITE}, 0 -1px 0 ${COLORS.WHITE},
-    1px 1px ${COLORS.WHITE}, -1px -1px 0 ${COLORS.WHITE}, 1px -1px 0 ${COLORS.WHITE}, -1px 1px 0 ${COLORS.WHITE};
+  background-size: 110%;
+  background-repeat: repeat;
+  background-image: linear-gradient(45deg, #f3ec78, #af4261, #475d99);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  -moz-background-clip: text;
+  -moz-text-fill-color: transparent;
 `
 
 export const NavBar: React.FC = () => {
@@ -37,7 +42,7 @@ export const NavBar: React.FC = () => {
 
   return (
     <StyledNav>
-      <StyledTitle>Sky Go</StyledTitle>
+      <StyledTitle>skyGo</StyledTitle>
       <StyledForm onSubmit={onSubmit}>
         <SearchBar onChange={onChange} />
         <SearchButton />
